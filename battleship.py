de02544dc606bcab9766274cd207b3efc6a88b07
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-04 13:53:27 +0800

    added print_grid() method to Map

--- battleship.py
+++ battleship.py
@@ -1,3 +1,15 @@
 class Map:
 	def __init__(self):
 		self.grid = [[' ' for x in range(10)] for y in range(10)]
+
+	def print_grid(self):
+		print("     A   B   C   D   E   F   G   H   I   J")
+		print("   -----------------------------------------")
+		for i in range(len(self.grid)):
+			if i > 8:
+				print(i + 1, end=' ')
+			else:
+				print(" {} ".format(i + 1), end='')
+			for square in self.grid[i]:
+				print("| {} ".format(square), end='')
+			print("|\n   -----------------------------------------")
