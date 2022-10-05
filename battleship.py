88a5f005598c3dcb11f01755cd7a830503c8878c
Riescent <91223304+Riescent@users.noreply.github.com>
2022-10-05 18:06:50 +0800

    Started add_boats() which will add boats on the grid where the user decides

--- battleship.py
+++ battleship.py
@@ -1,6 +1,64 @@
 class Map:
+	empty_symbol = ' '
+	boat_symbol = 'x'
+
 	def __init__(self):
-		self.grid = [[' ' for x in range(10)] for y in range(10)]
+		self.grid = [[self.empty_symbol for x in range(10)] for y in range(10)]
+		self.boats = []
+		self.add_boats()
+
+	def add_boats(self):
+		def cancel_last_placement():
+			print("To continue: press enter. To cancel: type anything")
+			if input() == '':
+				return False
+			return True
+
+		def add_boat(boat_size, boat_name, i):
+			def get_orientation():
+				print("To place the boat horizontaly enter h, to place it verticaly enter v")
+				orientation = input().lower()
+				if orientation != 'h' and orientation != 'v':
+					self.print_grid()
+					print("Incorrect input")
+					orientation = get_orientation()
+				return orientation
+
+			def remove_boat():
+				self.print_grid()
+				for coordinate in self.boats.pop(-1):
+					self.grid[coordinate['y']][coordinate['x']] = self.empty_symbol
+
+			def get_input(orientation):
+				pass
+
+
+			if i > 0 and cancel_last_placement():
+				remove_boat()
+				return i - 1
+
+			self.print_grid()
+			print("Placing a {name} of size {size}".format(name=boat_name, size=boat_size))
+			orientation = get_orientation()
+			input = get_input(orientation)
+
+			self.boats.append([{'y': y, 'x': x}])
+
+			for coordinate in self.boats[-1]:
+				self.grid[coordinate['y']][coordinate['x']] = self.boat_symbol
+
+		boats_to_add = [
+			[5, "Carrier"],
+			[4, "Battleship"],
+			[3, "Destroyer"],
+			[3, "Submarine"],
+			[2, "Patrol Boat"]
+		]
+		i = 0
+		while i < len(boats_to_add):
+			i = add_boat(boats_to_add[i][0], boats_to_add[i][1], i)
+			if i == len(boats_to_add) - 1 and cancel_last_placement(): i -= 1
+
 
 	def print_grid(self):
 		print("     A   B   C   D   E   F   G   H   I   J")
